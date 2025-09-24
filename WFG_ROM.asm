ba7f36b520f3047561e0151920f7743d8ea0632e
Oddbit Coder <oddbitcoder@gmail.com>
2025-09-24 16:20:26 +0200

    Consistency

--- WFG_ROM.asm
+++ WFG_ROM.asm
@@ -1,53 +1,53 @@
 ; ----------------------------------------
 
-	ORG	$0
+	ORG	$0000
 
 ; ----------------------------------------
 
-; TODO: NMI interrupt handler at $66
+; TODO: NMI interrupt handler at $0066
 
 	DI
 
-; Inicializacija PIO
-	LD	A,0x07
-	OUT	(0x31),A
-	OUT	(0x33),A
-	LD	A,0x0F
-	OUT	(0x31),A
-	OUT	(0x33),A
+; PIO initialization
+	LD	A,$07
+	OUT	($31),A
+	OUT	($33),A
+	LD	A,$0F
+	OUT	($31),A
+	OUT	($33),A
 
-	LD	A,0x18
-	OUT	(0x30),A
-	LD	A,0x6D
-	OUT	(0x32),A
+	LD	A,$18
+	OUT	($30),A
+	LD	A,$6D
+	OUT	($32),A
 	XOR	A
-	OUT	(0x39),A
-	OUT	(0x36),A
+	OUT	($39),A
+	OUT	($36),A
 	LD	E,A
 
-; Inicializacija GDP
+; GDP initialization
 	XOR	A
-	OUT	(0x21),A
+	OUT	($21),A
 
-; Izbira in spust GDP peresa
-	LD	A,0x03
-	OUT	(0x21),A
+; Select and lower GDP pen
+	LD	A,$03
+	OUT	($21),A
 
-	LD	A,0x04	; Pocxistimo GDP sliko...
+	LD	A,$04	; Clear GDP image...
 	LD HL,GDPUkaz_ret_1
 	JP	gdp_cmd
 GDPUkaz_ret_1:
-	LD	A,0x05	; ...in postavimo pero na levi rob.
+	LD	A,$05	; ...and place the pen at the left edge.
 	LD HL,GDPUkaz_ret_2
 	JP	gdp_cmd
 GDPUkaz_ret_2:
 	XOR	A
-	OUT	(0x39),A
-	OUT	(0x30),A
+	OUT	($39),A
+	OUT	($30),A
 
-; Inicializacija AVDC
-	LD	A,0x00
-	OUT	(0x39),A
+; AVDC initialization
+	LD	A,$00
+	OUT	($39),A
 	LD HL,Zakasnitev_ret
 	JP	Zakasnitev2
 Zakasnitev_ret:
@@ -55,46 +55,46 @@ Zakasnitev_ret:
 	XOR	A
 
 ; SS1 := 0
-	OUT	(0x3E),A
-	OUT	(0x3F),A
+	OUT	($3E),A
+	OUT	($3F),A
 
 ; SS2 := 0
-	OUT	(0x3A),A
-	OUT	(0x3B),A
+	OUT	($3A),A
+	OUT	($3B),A
 
-	LD	A,0x10
-	OUT	(0x39),A
-	LD	B,0x0A
-	LD	C,0x38
+	LD	A,$10
+	OUT	($39),A
+	LD	B,$0A
+	LD	C,$38
 	OTIR
 
-	LD	A,0x3D
-	OUT	(0x39),A
+	LD	A,$3D
+	OUT	($39),A
 
-; Naslov AVDC kurzorja := 0
+; AVDC cursor address := 0
 	XOR	A
-	OUT	(0x3D),A
-	OUT	(0x3C),A
+	OUT	($3D),A
+	OUT	($3C),A
 
-	LD	HL,0x1FFF
-	LD	A,0x1A
-	OUT	(0x39),A
+	LD	HL,$1FFF
+	LD	A,$1A
+	OUT	($39),A
 	LD	A,L
-	OUT	(0x38),A
+	OUT	($38),A
 	LD	A,H
-	OUT	(0x38),A
+	OUT	($38),A
 
-; Zapolni AVDC framebuffer s presledki?
-	LD	A,0x20
-	OUT	(0x34),A
+; Fill AVDC framebuffer with spaces?
+	LD	A,$20
+	OUT	($34),A
 	XOR	A
-	OUT	(0x35),A
-	LD	A,0xBB	; Write from cursor to pointer
-	OUT	(0x39),A
+	OUT	($35),A
+	LD	A,$BB	; Write from cursor to pointer
+	OUT	($39),A
 
-; Y koordinata GDP peresa := 100
-	LD	A,0x64
-	OUT	(0x2B),A
+; GDP pen Y coordinate := 100
+	LD	A,$64
+	OUT	($2B),A
 
 	LD HL,Prelom_ret
 	JP PrelomVrstice2
@@ -105,8 +105,8 @@ Prelom_ret:
 	JP	IzpisiNiz2
 IzpisiNiz_ret:
 
-; Spet postavimo pero na levi rob.
-	LD	A,0x05
+; Place the pen at the left edge again.
+	LD	A,$05
 	LD HL,GDPUkaz_ret
 	JP	gdp_cmd
 GDPUkaz_ret:
@@ -120,25 +120,25 @@ Prelom_ret_2:
 	JP	IzpisiNiz2
 IzpisiNiz_ret_3:
 
-	LD A,0
+	LD A,$00
 	LD HL,FillRamZero_ret
 	JP fill_ram
 FillRamZero_ret:
 
-	OUT	(0x90),A ; Switch to bank 2
+	OUT	($90),A ; Switch to bank 2
 
-	LD A,0
+	LD A,$00
 	LD HL,FillRamZero_ret_2
 	JP fill_ram
 FillRamZero_ret_2:
 
-	OUT	(0x88),A ; Switch back to bank 1
+	OUT	($88),A ; Switch back to bank 1
 
-	LD B,0
+	LD B,$00
 
 repeat_with_B1:
 
-	LD A,0
+	LD A,$00
 	EXX
 	LD HL,$2000
 	EXX
@@ -146,9 +146,9 @@ repeat_with_B1:
 	JP fill_ram_addr
 fill_ram_addr_ret:
 
-	OUT	(0x90),A ; Switch to bank 2
+	OUT	($90),A ; Switch to bank 2
 
-	LD A,1
+	LD A,$01
 	EXX
 	LD HL,$2000
 	EXX
@@ -156,9 +156,9 @@ fill_ram_addr_ret:
 	JP fill_ram_addr
 fill_ram_addr_ret_2:
 
-	OUT	(0x88),A ; Switch back to bank 1
+	OUT	($88),A ; Switch back to bank 1
 
-	LD A,0
+	LD A,$00
 	EXX
 	LD HL,$2000
 	EXX
@@ -166,9 +166,9 @@ fill_ram_addr_ret_2:
 	JP check_ram_addr
 check_ram_addr_ret:
 
-	OUT	(0x90),A ; Switch to bank 2
+	OUT	($90),A ; Switch to bank 2
 
-	LD A,1
+	LD A,$01
 	EXX
 	LD HL,$2000
 	EXX
@@ -176,11 +176,11 @@ check_ram_addr_ret:
 	JP check_ram_addr
 check_ram_addr_ret_2:
 
-	OUT	(0x88),A ; Switch back to bank 1
+	OUT	($88),A ; Switch back to bank 1
 
 	; TEST SHARED MEMORY
 
-	LD A,0
+	LD A,$00
 	EXX
 	LD HL,$C000
 	EXX
@@ -188,7 +188,7 @@ check_ram_addr_ret_2:
 	JP fill_ram_addr
 fill_ram_addr_ret_3:
 
-	LD A,0
+	LD A,$00
 	EXX
 	LD HL,$C000
 	EXX
@@ -196,9 +196,9 @@ fill_ram_addr_ret_3:
 	JP check_ram_addr
 check_ram_addr_ret_3:
 
-	OUT	(0x90),A ; Switch to bank 2
+	OUT	($90),A ; Switch to bank 2
 
-	LD A,0
+	LD A,$00
 	EXX
 	LD HL,$C000
 	EXX
@@ -206,7 +206,7 @@ check_ram_addr_ret_3:
 	JP check_ram_addr
 check_ram_addr_ret_4:
 
-	OUT	(0x88),A
+	OUT	($88),A
 
 	LD A,B
 	OR A
@@ -217,16 +217,16 @@ check_ram_addr_ret_4:
 	JP fill_ram
 FillRamZero_ret_3
 
-	OUT	(0x90),A ; Switch to bank 2
+	OUT	($90),A ; Switch to bank 2
 
 	LD A,$FF
 	LD HL,FillRamZero_ret_4
 	JP fill_ram
 FillRamZero_ret_4:
 
-	OUT	(0x88),A ; Switch back to bank 1
+	OUT	($88),A ; Switch back to bank 1
 
-	LD B,1
+	LD B,$01
 	JP repeat_with_B1
 
 done:
@@ -241,16 +241,16 @@ IzpisiNiz_ret_4:
 
 	HALT
 
-; Inicializacijski niz za AVDC.
+; AVDC initialization string.
 niz_init_avdc:
 	DB	$D0, $2F, $0D, $05, $99, $4F, $0A, $EA
 	DB	$00, $30
 
-; Inicializacijski niz za serijska vrata.
+; Serial port initialization string.
 niz_init_ser:
 	DB	$18, $04, $44, $03, $C1, $05, $68
 
-; Niz ukazov za GDP, ki premakne pero v spodnji levi kot in pobrisxe vrstico.
+; GDP command string that moves the pen to the bottom-left corner and clears the line.
 niz_premakni_pero_spodnji_levi_kot:
 	DB	$03, $00, $05, $01, $0B, $0B, $0B, $0B
 	DB	$0B, $0B, $0B, $0B, $0B, $0B, $0B, $0B
@@ -258,11 +258,11 @@ niz_premakni_pero_spodnji_levi_kot:
 
 ; ----------------------------------------
 
-; Niz ukazov za GDP, ki premakne pero na skrajno levo.
+; GDP command string that moves the pen to the far left.
 niz_premakni_pero_skrajno_levo:
 	DB	$21, $00, $0D, $00
 
-; Zagonsko sporocxilo.
+; Boot message.
 sporocilo_zagon:
 	DB	$A8, $00
 	DB	"Memory Test"
@@ -271,40 +271,40 @@ sporocilo_zagon:
 gdp_cmd:
 	EX	AF,AF'
 x00B3:
-	IN	A,(0x20)
-	AND	0x04
+	IN	A,($20)
+	AND	$04
 	JR	Z,x00B3
 	EX	AF,AF'
-	OUT	(0x20),A
+	OUT	($20),A
 	JP	(HL)
 
 Zakasnitev2:
-	LD	B,0xFF
+	LD	B,$FF
 x025D:
 	NOP
 	DJNZ	x025D
-	LD	B,0xFF
+	LD	B,$FF
 x025D_3:
 	NOP
 	DJNZ	x025D_3
-	LD	B,0xFF
+	LD	B,$FF
 x025D_4:
 	NOP
 	DJNZ	x025D_4
-	LD	B,0xFF
+	LD	B,$FF
 x025D_5:
 	NOP
 	DJNZ	x025D_5
 	JP (HL)
 
 PrelomVrstice2:
-	IN	A,(0x20)
-	AND	0x04
+	IN	A,($20)
+	AND	$04
 	JR	Z,PrelomVrstice2
 	LD	A,E
-	SUB	0x0C
+	SUB	$0C
 	LD	E,A
-	OUT	(0x36),A
+	OUT	($36),A
 	EXX
 	LD	BC,niz_premakni_pero_spodnji_levi_kot
 	LD HL,IzpisiNiz_ret_1
@@ -315,19 +315,19 @@ IzpisiNiz_ret_1:
 	JP	IzpisiNiz2
 IzpisiNiz_ret_2:
 	EXX
-; Izberemo nacxin risanja.
+; Select drawing mode.
 	XOR	A
 	JP	gdp_cmd
 
 IzpisiNiz2:
-	IN	A,(0x20)
-	AND	0x04
+	IN	A,($20)
+	AND	$04
 	JR	Z,IzpisiNiz2
 	LD	A,(BC)
-	OUT	(0x23),A
+	OUT	($23),A
 	INC	BC
 	LD	A,(BC)
-	OUT	(0x22),A
+	OUT	($22),A
 	INC	BC
 x0251_2:
 	LD	A,(BC)
@@ -335,11 +335,11 @@ x0251_2:
 	JR NZ,skip
 	JP (HL)
 skip:
-	IN	A,(0x20)
-	AND	0x04
+	IN	A,($20)
+	AND	$04
 	JR	Z,skip
 	LD A,(BC)
-	OUT	(0x20),A
+	OUT	($20),A
 	INC	BC
 	JR	x0251_2
 
@@ -364,46 +364,46 @@ write_hex_8:
 	SRA	A
 	SRA	A
 	SRA	A
-	AND	0x0F
+	AND	$0F
 
-	CP	0x0A
+	CP	$0A
 	JP	M,x0099_2
-	ADD	A,0x37
+	ADD	A,$37
 	JR	x009B_2
 x0099_2:
-	ADD	A,0x30
+	ADD	A,$30
 x009B_2:
 	EX AF,AF'
 x009B_2_2:
-	IN	A,(0x20)
-	AND	0x04
+	IN	A,($20)
+	AND	$04
 	JR	Z,x009B_2_2
 	EX	AF,AF'
-	OUT	(0x20),A
+	OUT	($20),A
 
 	LD	A,B
-	AND	0x0F
+	AND	$0F
 
-	CP	0x0A
+	CP	$0A
 	JP	M,x0099_3
-	ADD	A,0x37
+	ADD	A,$37
 	JR	x009B_3
 x0099_3:
-	ADD	A,0x30
+	ADD	A,$30
 x009B_3:
 	EX AF,AF'
 x009B_3_2:
-	IN	A,(0x20)
-	AND	0x04
+	IN	A,($20)
+	AND	$04
 	JR	Z,x009B_3_2
 	EX	AF,AF'
-	OUT	(0x20),A
+	OUT	($20),A
 
 	JP (HL)
 
 ; ----------------------------------------
 
-; Vstopna tocxka v memory test.
+; Entry point to memory test.
 
 ; fills RAM $2000-$FFFF with what's in A
 fill_ram:
@@ -416,7 +416,7 @@ fill_ram_loop:
 	LD A,H
 	OR L
 	JR NZ,fill_ram_loop
-	LD A,'+'
+	LD A,$2B
 	LD HL,fill_ram_gdp_cmd_ret
 	JP gdp_cmd
 fill_ram_gdp_cmd_ret:
@@ -430,7 +430,7 @@ fill_ram_addr:
 	; HL is passed in by the caller
 	LD E,A
 fill_ram_addr_loop:
-	; check if 0 or FF before writing
+	; check if $00 or $FF before writing
 	LD A,(HL)
 	LD D,A
 	EXX
@@ -470,7 +470,7 @@ fill_ram_addr_not_C000:
 	OR L
 	JR NZ,fill_ram_addr_loop
 fill_ram_addr_done:
-	LD A,'*'
+	LD A,$2A
 	LD HL,fill_ram_addr_gdp_cmd_ret
 	JP gdp_cmd
 fill_ram_addr_gdp_cmd_ret:
@@ -478,13 +478,13 @@ fill_ram_addr_gdp_cmd_ret:
 	JP (HL)
 fill_ram_addr_fail:
 	; HL - address
-	; 0 - expected
+	; $00 - expected
 	; A - actual
 	LD E,A
 	LD B,H
 	LD C,L
 	; print address
-	LD A,' '
+	LD A,$20
 	LD HL,fill_ram_addr_gdp_cmd_ret_2
 	JP gdp_cmd
 fill_ram_addr_gdp_cmd_ret_2:
@@ -492,16 +492,16 @@ fill_ram_addr_gdp_cmd_ret_2:
 	JP write_hex_16
 fill_ram_addr_write_hex_16_ret:
 	; print expected
-	LD A,' '
+	LD A,$20
 	LD HL,fill_ram_addr_gdp_cmd_ret_3
 	JP gdp_cmd
 fill_ram_addr_gdp_cmd_ret_3:
-	LD A,0
+	LD A,$00
 	LD HL,fill_ram_addr_write_hex_8_ret
 	JP write_hex_8
 fill_ram_addr_write_hex_8_ret:
 	; print actual
-	LD A,' '
+	LD A,$20
 	LD HL,fill_ram_addr_gdp_cmd_ret_4
 	JP gdp_cmd
 fill_ram_addr_gdp_cmd_ret_4:
@@ -548,7 +548,7 @@ check_ram_addr_not_C000:
 	OR L
 	JR NZ,check_ram_addr_loop
 check_ram_addr_done:
-	LD A,'*'
+	LD A,$2A
 	LD HL,check_ram_addr_gdp_cmd_ret
 	JP gdp_cmd
 check_ram_addr_gdp_cmd_ret:
@@ -563,7 +563,7 @@ check_ram_addr_fail:
 	LD B,H
 	LD C,L
 	; print address
-	LD A,' '
+	LD A,$20
 	LD HL,check_ram_addr_gdp_cmd_ret_2
 	JP gdp_cmd
 check_ram_addr_gdp_cmd_ret_2:
@@ -571,7 +571,7 @@ check_ram_addr_gdp_cmd_ret_2:
 	JP write_hex_16
 check_ram_addr_write_hex_16_ret:
 	; print expected
-	LD A,' '
+	LD A,$20
 	LD HL,check_ram_addr_gdp_cmd_ret_3
 	JP gdp_cmd
 check_ram_addr_gdp_cmd_ret_3:
@@ -580,7 +580,7 @@ check_ram_addr_gdp_cmd_ret_3:
 	JP write_hex_8
 check_ram_addr_write_hex_8_ret:
 	; print actual
-	LD A,' '
+	LD A,$20
 	LD HL,check_ram_addr_gdp_cmd_ret_4
 	JP gdp_cmd
 check_ram_addr_gdp_cmd_ret_4:
@@ -592,7 +592,7 @@ check_ram_addr_write_hex_8_ret_2:
 
 ; ----------------------------------------
 
-; Sporocxilo o testiranju spomina.
+; Memory testing message.
 sporocilo_testing_memory:
 	DB	$21, $00
 	DB	"TESTING "
