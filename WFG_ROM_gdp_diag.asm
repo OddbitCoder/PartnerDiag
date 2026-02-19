6fc06dd10de1e065933f75091431170cd2c653e7
Oddbit Coder <oddbitcoder@gmail.com>
2026-02-19 17:17:57 +0100

    Add AVDC ROM helper subroutines for reset and clear flow

--- WFG_ROM_gdp_diag.asm
+++ WFG_ROM_gdp_diag.asm
@@ -54,12 +54,12 @@ delay_ret:
 	XOR	A
 
 ; Set SS1 to 0.
-	OUT	($3E), A
-	OUT	($3F), A
-
-; Set SS2 to 0.
 	OUT	($3A), A
 	OUT	($3B), A
+
+; Set SS2 to 0.
+	OUT	($3E), A
+	OUT	($3F), A
 
 	LD	A, $10
 	OUT	($39), A
@@ -146,162 +146,201 @@ line_break_ret_2:
 	JP	write_string
 write_string_ret_2:
 
-; GDP diagnostics steps.
-; Test block 1
-	LD	A, $07
-	OUT	($33), A
-	LD	A, $07
-	OUT	($31), A
-	LD	A, $B0
-	OUT	($31), A
-	LD	A, $B2
-	OUT	($33), A
-	LD	A, $07
-	OUT	($31), A
-	LD	A, $0F
-	OUT	($31), A
+; GDP diagnostics.
+	CALL	avdc_reset
 	LD	A, '*'
 	CALL	write_char
 	LD	HL, 1000
 	CALL	msleep
 
-; Test block 2
-	LD	A, $07
-	OUT	($33), A
-	LD	A, $0F
-	OUT	($33), A
-	LD	A, $07
-	OUT	($33), A
+	CALL avdc_clear_screen
 	LD	A, '*'
 	CALL	write_char
 	LD	HL, 1000
 	CALL	msleep
-
-; Test block 3
-	LD	A, $65
-	OUT	($32), A
-	XOR	A
-	OUT	($39), A
-	OUT	($39), A
-	LD	A, '*'
-	CALL	write_char
-	LD	HL, 1000
-	CALL	msleep
-
-; Test block 4
-	XOR	A
-	OUT	($3E), A
-	OUT	($3F), A
-	LD	A, '*'
-	CALL	write_char
-	LD	HL, 1000
-	CALL	msleep
-
-; Test block 5
-	LD	A, $10
-	OUT	($39), A
-	LD	A, $D0
-	OUT	($38), A
-	LD	A, $2F
-	OUT	($38), A
-	LD	A, $8D
-	OUT	($38), A
-	LD	A, $05
-	OUT	($38), A
-	LD	A, $99
-	OUT	($38), A
-	LD	A, $4F
-	OUT	($38), A
-	LD	A, $0A
-	OUT	($38), A
-	LD	A, $EA
-	OUT	($38), A
-	XOR	A
-	OUT	($38), A
-	LD	A, $30
-	OUT	($38), A
-	LD	A, '*'
-	CALL	write_char
-	LD	HL, 1000
-	CALL	msleep
-
-; Test block 6
-	LD	A, $1C
-	OUT	($39), A
-	XOR	A
-	OUT	($38), A
-	LD	A, $17
-	OUT	($38), A
-	XOR	A
-	OUT	($38), A
-	LD	A, '*'
-	CALL	write_char
-	LD	HL, 1000
-	CALL	msleep
-
-; Test block 7
-	LD	A, $3D
-	OUT	($39), A
-	XOR	A
-	OUT	($3D), A
-	OUT	($3C), A
-	LD	A, '*'
-	CALL	write_char
-	LD	HL, 1000
-	CALL	msleep
-
-; Test block 7.5 (delayed command sanity check)
-	LD	A, $A9
-	OUT	($39), A
-	LD	A, '*'
-	CALL	write_char
-	LD	HL, 1000
-	CALL	msleep
-
-; Test block 8 ; THIS FAILS - BAD MEMORY?
-;	LD	A, $1A
-;	OUT	($39), A
-;	LD	A, $FF
-;	OUT	($38), A
-;	LD	A, $0F
-;	OUT	($38), A
-;	LD	A, $20
-;	OUT	($34), A
-;	XOR	A
-;	OUT	($35), A
-;	LD	A, $BB
-;	OUT	($39), A
-;	LD	A, '*'
-;	CALL	write_char
-;	LD	HL, 1000
-;	CALL	msleep
-
-; Test block 9 (explicit code from trace)
-test_wait_39_ready_1:
-	IN	A, ($39)
-	AND	$20
-	JR	Z, test_wait_39_ready_1
-test_wait_36_access:
-	EI
-	IN	A, ($36)
-	DI
-	AND	$10
-	JR	NZ, test_wait_36_access
-test_wait_39_ready_2:
-	IN	A, ($39)
-	AND	$20
-	JR	Z, test_wait_39_ready_2
-	LD	A, '*'
-	CALL	write_char
-	LD	HL, 1000
-	CALL	msleep
-
-; More tests go here.
 
 	LD	A, '#'
 	CALL	write_char
 
 	HALT
+
+; Wait until AVDC status READY flag is set.
+avdc_wait_ready:
+avdc_wait_ready_loop:
+	IN	A, ($39)
+	AND	$20
+	JR	Z, avdc_wait_ready_loop
+	RET
+
+; Wait for one full AVDC access window.
+avdc_wait_access:
+avdc_wait_access_high:
+	IN	A, ($36)
+	AND	$10
+	JR	Z, avdc_wait_access_high
+avdc_wait_access_low:
+	IN	A, ($36)
+	AND	$10
+	JR	NZ, avdc_wait_access_low
+	RET
+
+; Wait for completion of a delayed AVDC command.
+; Repeats access-window sync until READY is observed.
+avdc_wait_long_command:
+avdc_wait_long_command_loop:
+	CALL	avdc_wait_access
+	IN	A, ($39)
+	AND	$20
+	JR	Z, avdc_wait_long_command_loop
+	RET
+
+; Set AVDC cursor address from HL.
+avdc_set_cursor_addr:
+	CALL	avdc_wait_access
+	CALL	avdc_wait_ready
+	LD	A, L
+	OUT	($3C), A
+	LD	A, H
+	OUT	($3D), A
+	RET
+
+; Write HL as a 16-bit address at current cursor using WRITE_AT_CUR.
+avdc_write_addr_at_cursor:
+	CALL	avdc_wait_access
+	CALL	avdc_wait_ready
+	LD	A, L
+	OUT	($34), A
+	XOR	A
+	OUT	($35), A
+	LD	A, $AB
+	OUT	($39), A
+	CALL	avdc_wait_ready
+	LD	A, H
+	OUT	($34), A
+	XOR	A
+	OUT	($35), A
+	LD	A, $AB
+	OUT	($39), A
+	RET
+
+; Clear one AVDC text row.
+; IN: HL = row start address (row * 132 + 450)
+avdc_clear_row:
+	PUSH	HL
+	CALL	avdc_wait_access
+	CALL	avdc_wait_ready
+
+; Set cursor.
+	LD	A, L
+	OUT	($3C), A
+	LD	A, H
+	OUT	($3D), A
+
+; Set pointer to row end (addr + 131).
+	LD	DE, 131
+	ADD	HL, DE
+	LD	A, $1A
+	OUT	($39), A
+	LD	A, L
+	OUT	($38), A
+	LD	A, H
+	OUT	($38), A
+
+; Write cursor to pointer.
+	LD	A, 'M'
+	OUT	($34), A
+	XOR	A
+	OUT	($35), A
+	LD	A, $BB
+	OUT	($39), A
+	CALL	avdc_wait_long_command
+	POP	HL
+	RET
+
+; Clear full AVDC text screen (26 rows).
+avdc_clear_screen:
+	LD	HL, 450
+	LD	B, 26
+avdc_clear_screen_loop:
+	PUSH	BC
+	PUSH	HL
+	CALL	avdc_clear_row
+	POP	HL
+	LD	DE, 132
+	ADD	HL, DE
+	POP	BC
+	DJNZ	avdc_clear_screen_loop
+	RET
+
+; Reset and initialize AVDC.
+avdc_reset:
+	CALL	avdc_wait_access
+	CALL	avdc_wait_ready
+	XOR	A
+	OUT	($39), A
+avdc_reset_wait_access_low:
+	IN	A, ($36)
+	AND	$10
+	JR	NZ, avdc_reset_wait_access_low
+	XOR	A
+	OUT	($39), A
+
+; Set common text attributes.
+	LD	A, $65
+	OUT	($32), A
+
+; Wait for V blanking edge on GDP status bit 1.
+avdc_reset_wait_vblank_high:
+	IN	A, ($20)
+	AND	$02
+	CP	$02
+	JR	NZ, avdc_reset_wait_vblank_high
+avdc_reset_wait_vblank_low:
+	IN	A, ($20)
+	AND	$02
+	CP	$02
+	JR	Z, avdc_reset_wait_vblank_low
+
+; Set screen start 2.
+	XOR	A
+	OUT	($3E), A
+	OUT	($3F), A
+
+; Write init register sequence.
+	LD	A, $10
+	OUT	($39), A
+	LD	HL, data_avdc_init_reset
+	LD	B, 10
+avdc_reset_init_loop:
+	LD	A, (HL)
+	OUT	($38), A
+	INC	HL
+	DJNZ	avdc_reset_init_loop
+
+; Set screen start 2 again.
+	XOR	A
+	OUT	($3E), A
+	OUT	($3F), A
+
+; Turn display back on.
+	LD	A, $3D
+	OUT	($39), A
+
+; Write row table.
+	LD	HL, 0
+	CALL	avdc_set_cursor_addr
+	LD	HL, 450
+	LD	B, 26
+avdc_reset_table_loop:
+	PUSH	BC
+	PUSH	HL
+	CALL	avdc_write_addr_at_cursor
+	POP	HL
+	LD	DE, 132
+	ADD	HL, DE
+	POP	BC
+	DJNZ	avdc_reset_table_loop
+	RET
 
 ; Sleep HL milliseconds.
 msleep:
@@ -326,6 +365,11 @@ write_char_ret:
 ; AVDC initialization string.
 data_avdc_init:
 	DB	$D0, $2F, $0D, $05, $99, $4F, $0A, $EA
+	DB	$00, $30
+
+; AVDC init sequence used by avdc_reset.
+data_avdc_init_reset:
+	DB	$D0, $2F, $8D, $05, $99, $4F, $0A, $EA
 	DB	$00, $30
 
 ; GDP command string that moves the pen to the bottom-left corner and clears the line.
